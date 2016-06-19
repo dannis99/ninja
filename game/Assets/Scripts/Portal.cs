8422b925564efaf57e3c3bfc7440d4e751ab88b1
Daniel Harrington <danieljonharrington@gmail.com>
2016-06-19 10:57:56 -0600

    adding portals

---
+++ game/Assets/Scripts/Portal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour
+{
+    public SpriteRenderer portalRenderer;
+    public Collider2D portalCollider;
+    public Portal pairedPortal;
+    public GameObject portaledObject;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject currObject = collider.gameObject;
+        if (currObject == portaledObject)
+        {
+            portaledObject = null;
+        }
+        else
+        {
+            pairedPortal.portaledObject = currObject;
+            currObject.transform.position = pairedPortal.transform.position;
+        }
+    }
+}
