c318f9f4c227261a71e770c50e2659e0eec5f483
Daniel Harrington <danieljonharrington@gmail.com>
2016-06-24 23:00:55 -0600

    adding the bouncing/penetrating shurikens and allowing dodging of shurikens

---
+++ game/Assets/Scripts/PenetratingShurikenController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PenetratingShurikenController : ShurikenParentController {
+
+	bool active = true;
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(active)
+		{
+			if(collider.gameObject.tag == "player")
+			{
+				collider.gameObject.GetComponent<PlayerController>().takeDamage();
+			}
+			else
+			{
+                active = false;
+                velocity = Vector2.zero;
+				shurikenRigidbody2D.isKinematic = true;
+
+				foreach(Collider2D currCollider in colliders)
+				{
+					currCollider.enabled = false;
+				}
+			}
+		}
+	}
+}
