26964b5e37dc9eab88c21b43232f9bf8a1aae36a
Daniel Harrington <danieljonharrington@gmail.com>
2016-06-26 00:24:22 -0600

    fixing a bunch of stuff with slime grenades

--- game/Assets/Scripts/SlimeController.cs
+++ game/Assets/Scripts/SlimeController.cs
@@ -4,19 +4,43 @@ using System.Collections;
 public class SlimeController : MonoBehaviour {
 
     public float duration;
+    public GameObject groundSlimePrefab;
+
+    private float elapsedTime;
 
     void Start()
     {
         Invoke("destroySlime", duration);
     }
 
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+    }
+
 	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "player")
-		{
-            collision.gameObject.GetComponent<PlayerController> ().takeDamage ();			
-		}
-	}
+    {
+        detectCollision(collision);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        detectCollision(collision);
+    }
+
+    private void detectCollision(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().takeDamage();
+        }
+        else if (elapsedTime > .2f && collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("Walls"))
+        {
+            GameObject groundSlime = Instantiate<GameObject>(groundSlimePrefab);
+            groundSlime.transform.position = collision.contacts[0].point;
+            Destroy(gameObject);
+        }
+    }
 
     private void destroySlime()
     {
---
+++ game/Assets/Scripts/SlimeGroundController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeGroundController : MonoBehaviour {
+
+    public float duration;
+    
+    void Start()
+    {
+        Invoke("destroySlime", duration);
+    }
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "player")
+		{
+            collider.gameObject.GetComponent<PlayerController> ().takeDamage ();			
+		}
+	}
+
+    private void destroySlime()
+    {
+        Destroy(gameObject);
+    }
+}
