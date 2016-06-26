704c65401860c3ccf80fcf9ef02ab2a04714bffb
Daniel Harrington <danieljonharrington@gmail.com>
2016-06-26 12:14:29 -0600

    fixing slime to wall orientation

--- game/Assets/Scripts/SlimeController.cs
+++ game/Assets/Scripts/SlimeController.cs
@@ -38,6 +38,18 @@ public class SlimeController : MonoBehaviour {
         {
             GameObject groundSlime = Instantiate<GameObject>(groundSlimePrefab);
             groundSlime.transform.position = collision.contacts[0].point;
+            if(collision.gameObject.layer == LayerMask.NameToLayer("Walls"))
+            {
+                if (transform.position.x < collision.gameObject.transform.position.x)
+                    groundSlime.transform.eulerAngles = new Vector3(groundSlime.transform.eulerAngles.x, groundSlime.transform.eulerAngles.y, 90);
+                else
+                    groundSlime.transform.eulerAngles = new Vector3(groundSlime.transform.eulerAngles.x, groundSlime.transform.eulerAngles.y, -90);
+            }
+            else
+            {
+                if(transform.position.y < collision.gameObject.transform.position.y)
+                    groundSlime.transform.eulerAngles = new Vector3(groundSlime.transform.eulerAngles.x, groundSlime.transform.eulerAngles.y, 180);
+            }
             Destroy(gameObject);
         }
     }
--- game/Assets/Scripts/SlimeGrenadeController.cs
+++ game/Assets/Scripts/SlimeGrenadeController.cs
@@ -20,7 +20,12 @@ public class SlimeGrenadeController : GrenadeController {
             GameObject slime = Instantiate<GameObject>(slimePrefab);
             slime.transform.position = transform.position;
             Rigidbody2D slimeBody = slime.GetComponent<Rigidbody2D>();
-            slimeBody.AddForce(new Vector2(Random.Range(-maxForce, maxForce), Random.Range(-maxForce, maxForce)), ForceMode2D.Impulse);
+            float xForce = Random.Range(.5f, maxForce);
+            xForce = (Random.Range(1, 3) == 2) ? -xForce : xForce;
+            float yForce = Random.Range(.5f, maxForce);
+            Debug.Log("random:" + Random.Range(1, 2));
+            Debug.Log("slime force: " + xForce + "," + yForce);
+            slimeBody.AddForce(new Vector2(xForce, yForce), ForceMode2D.Impulse);
         }
         destroyGrenade();
 	}
