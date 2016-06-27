9244fce1ba9486264443d97daf4ad7bd267ffd0e
Daniel Harrington <danieljonharrington@gmail.com>
2016-06-26 23:06:11 -0600

    starting some floating platform stuff

---
+++ game/Assets/Scripts/FloatingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FloatingPlatform : MonoBehaviour {
+
+	public Vector2 startingPosition;
+    public Vector2 endPosition;
+    public float speed;
+
+	bool movingToEnd = true;
+	float t;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(movingToEnd && (transform.position.x != endPosition.x || transform.position.y != endPosition.y))
+		{
+			t += Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);//new Vector3(Mathf.Lerp(startingPosition.x, endPosition.x, t/movementDuration), Mathf.Lerp(startingPosition.y, endPosition.y, t/movementDuration), transform.position.z);			
+		}
+		else if(!movingToEnd && (transform.position.x != startingPosition.x || transform.position.y != startingPosition.y))
+        {
+			t += Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, startingPosition, speed * Time.deltaTime);//transform.position = new Vector3(Mathf.Lerp(endPosition.x, startingPosition.x, t/movementDuration), Mathf.Lerp(endPosition.y, startingPosition.y, t/movementDuration), transform.position.z);
+        }
+
+		if((transform.position.x == startingPosition.x && transform.position.y == startingPosition.y) ||
+           (transform.position.x == endPosition.x && transform.position.y == endPosition.y))
+		{
+			t = 0;
+            movingToEnd = (movingToEnd) ? false : true;
+		}
+	}
+}
