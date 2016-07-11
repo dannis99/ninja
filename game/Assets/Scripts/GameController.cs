7b426d890759358dc139bbe58afc0a0d9f3b974d
Daniel Harrington <danieljonharrington@gmail.com>
2016-07-10 23:26:59 -0600

    adding the ability to randomly generate chests

--- game/Assets/Scripts/GameController.cs
+++ game/Assets/Scripts/GameController.cs
@@ -9,15 +9,28 @@ class GameController : MonoBehaviour
 {
     public float roundLength;
     public float warningTime;
+
+    public float chestTimerMin;
+    public float chestTimerMax;
+    bool allChestsShown;
+    float chestTimer;
+
     float gameTime;
     PlayerController[] players;
     WarningLight[] warningLights;
+    Chest[] chests;
     bool reloadingScene;
 
     void Start()
     {
         players = Object.FindObjectsOfType<PlayerController>();
         warningLights = Object.FindObjectsOfType<WarningLight>();
+        chests = Object.FindObjectsOfType<Chest>();
+        foreach(Chest chest in chests)
+        {
+            chest.gameObject.SetActive(false);
+        }
+        chestTimer = Random.Range(chestTimerMin, chestTimerMax);        
     }
 
     void Update()
@@ -29,6 +42,36 @@ class GameController : MonoBehaviour
             {
                 warningLight.beginWarning();
             }
+        }
+
+        if(!allChestsShown && gameTime >= chestTimer)
+        {
+            int chestIndex = Random.Range(0, chests.Length - 1);
+            int chestsChecked = 0;
+            for(int i = chestIndex; i < chests.Length; i++)
+            {
+                Debug.Log("i: "+i);
+                if (!chests[i].gameObject.activeSelf)
+                {
+                    chests[i].gameObject.SetActive(true);
+                    break;
+                }
+                else if (i == chestIndex && chestsChecked > 0)
+                {
+                    allChestsShown = true;
+                    break;
+                }
+                else if(i == chests.Length - 1)
+                {
+                    i = -1;
+                }
+                
+                chestsChecked++;
+                if (chestsChecked >= chests.Length)
+                    break;
+            }
+            chestTimer += Random.Range(chestTimerMin, chestTimerMax);
+            Debug.Log("chestTimer: " + chestTimer + " gameTime: " + gameTime);
         }
     }
 
